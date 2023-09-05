c8d386b76d07b8c7fa63497489c8447886afecaf
jenny0263 <jenny0263@naver.com>
2023-09-05 11:53:39 +0900

    Modify count.cc

--- count.cc
+++ count.cc
@@ -3,4 +3,12 @@
 void count_number() {
     // lab2 //
     // do it yourself //
+    using namespace std;
+
+         int cnt;
+         for (cnt = 1; cnt < 11; cnt++) {
+                      cout << cnt << '\n';
+                           
+         }
+              cout << "2020311230" << endl;
 }
